0c256d3797bb46c644b19d78ff8296636b31e901
An Ba <ngwin.ab@gmail.com>
2024-02-09 00:24:06 -0600

    first commit

---
+++ src/main/java/com/anba/bakery/BakeryTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.anba.bakery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BakeryTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BakeryTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anba/bakery/BakeryTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anba.bakery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BakeryTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
