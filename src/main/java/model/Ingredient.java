8ea3d9b5cfe1c3933832e30b6b75446bf79ab0b0
An Ba <ngwin.ab@gmail.com>
2024-02-10 01:45:54 -0600

    update IngredientController

--- src/main/java/model/Ingredient.java
+++ src/main/java/model/Ingredient.java
@@ -61,4 +61,11 @@ public class Ingredient {
     public String getCategory() {
         return this.category;
     }
+
+    public void setCategory(String category)
+    {
+        this.category = category;
+    }
 }
+
+
